2fd562f42c9024f866fa4aa125a774ac99a89e57
Kellie Werrell <kelliewerrell@gmail.com>
2021-08-26 18:56:14 -0700

    changed resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "../../App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import resumePDF from "../../assets/docs/DevResume_2021.pdf";
+import resumePDF from "../../assets/docs/2021DevResume.pdf";
 
 function Resume() {
   return (
